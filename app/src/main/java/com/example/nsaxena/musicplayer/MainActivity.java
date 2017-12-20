f30b1bb53b043c0491c8a085339da5244d01c0b9
Nihal Saxena <root@localhost.localdomain>
2017-12-20 21:49:51 +0530

    design fix

--- app/src/main/java/com/example/nsaxena/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/nsaxena/musicplayer/MainActivity.java
@@ -1,10 +1,18 @@
 package com.example.nsaxena.musicplayer;
 
+import android.Manifest;
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -15,12 +23,21 @@ import android.widget.ListView;
 import android.widget.SeekBar;
 
 
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 
-public class MainActivity extends Activity implements GetJsonData.OnDataAvailable{
+public class MainActivity extends Activity implements GetJsonData.OnDataAvailable {
 
     private static final String TAG = "MainActivity";
 
@@ -38,9 +55,12 @@ public class MainActivity extends Activity implements GetJsonData.OnDataAvailabl
     private MediaPlayer mMediaPlayer;
 
     private SeekBar mSeekBar;
-    int rowSize = 1;
 
-    Handler mHandler ;
+    ProgressDialog mProgressDialog;
+
+    int rowSize = 4;
+
+    Handler mSeekBarHandler, mDownloadManagerHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,13 +69,15 @@ public class MainActivity extends Activity implements GetJsonData.OnDataAvailabl
 
         setContentView(R.layout.activity_main);
 
-        mHandler = new Handler();
+        mSeekBarHandler = new Handler();
+
+        mDownloadManagerHandler = new Handler();
 
         mLinearScroll = (LinearLayout) findViewById(R.id.linear_scroll);
 
-        RecyclerView recyclerView =(RecyclerView)findViewById(R.id.recycle_view);
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycle_view);
 
-        mSeekBar=(SeekBar)findViewById(R.id.seekbar) ;
+        mSeekBar = (SeekBar) findViewById(R.id.seekbar);
 
         mArrayListSong = new ArrayList<Song>();
 
@@ -63,47 +85,47 @@ public class MainActivity extends Activity implements GetJsonData.OnDataAvailabl
 
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        mRecyclerViewAdapter = new RecyclerViewAdapter(new ArrayList<Song>(),this);
+        mRecyclerViewAdapter = new RecyclerViewAdapter(new ArrayList<Song>(), this);
         recyclerView.setAdapter(mRecyclerViewAdapter);
 
         mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
 
             boolean userTouch;
+
             @Override
             public void onStopTrackingTouch(SeekBar arg0) {
                 userTouch = false;
             }
+
             @Override
             public void onStartTrackingTouch(SeekBar arg0) {
                 userTouch = true;
             }
+
             @Override
             public void onProgressChanged(SeekBar arg0, int arg1, boolean arg2) {
-                if(mMediaPlayer!=null) {
+                if (mMediaPlayer != null) {
                     if (mMediaPlayer.isPlaying() && userTouch)
                         mMediaPlayer.seekTo(arg1);
 
                 }
             }
         });
-        
+
         mRecyclerViewAdapter.setOnItemClickListener(new RecyclerViewAdapter.OnItemClickListener() {
             @Override
             public void onItemClick(final Button button, View view, final Song songObject, int position) {
-                Runnable r =new Runnable() {
+                Runnable r = new Runnable() {
                     @Override
                     public void run() {
                         try {
-                            if(button.getText().toString().equals("Stop"))
-                            {
+                            if (button.getText().toString().equals("Stop")) {
                                 button.setText("Play");
                                 mMediaPlayer.stop();
                                 mMediaPlayer.reset();
                                 mMediaPlayer.release();
-                                mMediaPlayer=null;
-                            }
-                            else
-                            {
+                                mMediaPlayer = null;
+                            } else {
                                 mMediaPlayer = new MediaPlayer();
                                 mMediaPlayer.setDataSource(songObject.getSongUrl());
                                 mMediaPlayer.prepareAsync();
@@ -113,17 +135,17 @@ public class MainActivity extends Activity implements GetJsonData.OnDataAvailabl
                                         mediaPlayer.start();
                                         mSeekBar.setProgress(0);
                                         mSeekBar.setMax(mediaPlayer.getDuration());
-
                                     }
                                 });
                                 button.setText("Stop");
                             }
-                        }catch (IOException e){
+                        } catch (IOException e) {
 
                         }
                     }
                 };
-                mHandler.postDelayed(r,100);
+                mSeekBarHandler.postDelayed(r, 100);
+
             }
         });
 
@@ -131,18 +153,16 @@ public class MainActivity extends Activity implements GetJsonData.OnDataAvailabl
         seekBarThread.run();
     }
 
-    public class SeekBarThread extends Thread
-    {
+    public class SeekBarThread extends Thread {
         @Override
         public void run() {
-            try{
+            try {
                 Thread.sleep(1000);
-                if(mMediaPlayer!=null) {
+                if (mMediaPlayer != null) {
                     mSeekBar.setProgress(mMediaPlayer.getCurrentPosition());
                 }
 
-            }
-            catch (InterruptedException e){
+            } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
@@ -153,16 +173,14 @@ public class MainActivity extends Activity implements GetJsonData.OnDataAvailabl
         LinearLayout ll = (LinearLayout) findViewById(R.id.linear_scroll);
         ll.removeAllViews();
         super.onResume();
-        GetJsonData getJsonData = new GetJsonData("http://starlord.hackerearth.com/studio",this);
+        GetJsonData getJsonData = new GetJsonData("http://starlord.hackerearth.com/studio", this);
         getJsonData.executeOnSameThread();
     }
 
     @Override
-    public void onDataAvailable(List<Song> data, DownloadStatus status)
-    {
+    public void onDataAvailable(List<Song> data, DownloadStatus status) {
 
-        if(status==DownloadStatus.OK)
-        {
+        if (status == DownloadStatus.OK) {
             int rem = data.size() % rowSize;
             mArrayListSong = new ArrayList<Song>(data);
 
@@ -172,7 +190,7 @@ public class MainActivity extends Activity implements GetJsonData.OnDataAvailabl
             addItem(0);
 
             int size = mArrayListSong.size() / rowSize;
-            Log.d(TAG, "onDataAvailable: data "+mArrayListSong.size());
+            Log.d(TAG, "onDataAvailable: data " + mArrayListSong.size());
             for (int j = 0; j < size; j++) {
                 final int k;
                 k = j;
@@ -200,15 +218,15 @@ public class MainActivity extends Activity implements GetJsonData.OnDataAvailabl
                 });
             }
             //mRecyclerViewAdapter.loadNewData(data);
-        }else
-        {
-            Log.e(TAG, "onDownloadComplete: failed with status "+status );
+        } else {
+            Log.e(TAG, "onDownloadComplete: failed with status " + status);
         }
 
         Log.d(TAG, "onDataAvailable: ends");
     }
+
     public void addItem(int count) {
-        Log.d(TAG, "addItem: "+mArrayListSongTemp.toString());
+        Log.d(TAG, "addItem: " + mArrayListSongTemp.toString());
         mArrayListSongTemp.clear();
         count = count * rowSize;
         /**
--- app/src/main/java/com/example/nsaxena/musicplayer/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/nsaxena/musicplayer/RecyclerViewAdapter.java
@@ -14,6 +14,7 @@ import com.bumptech.glide.Glide;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.bumptech.glide.load.resource.bitmap.CircleCrop;
 import com.bumptech.glide.request.RequestOptions;
 
 
@@ -67,8 +68,7 @@ class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.Image
         holder.play.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(mOnItemClickListener!=null)
-                {
+                if(mOnItemClickListener!=null){
                     mOnItemClickListener.onItemClick(holder.play,view,songItem,position);
                 }
             }
